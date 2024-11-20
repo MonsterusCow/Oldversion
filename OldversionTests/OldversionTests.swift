b88e2c5b15270d80832896390ec51ccbf58b7e0d
MonsterusCow <ryan.stark26@d155.org>
2024-11-20 07:49:38 -0600

    Initial Commit

---
+++ OldversionTests/OldversionTests.swift
@@ -0,0 +1,17 @@
+//
+//  OldversionTests.swift
+//  OldversionTests
+//
+//  Created by RYAN STARK on 11/20/24.
+//
+
+import Testing
+@testable import Oldversion
+
+struct OldversionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
